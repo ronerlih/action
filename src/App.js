37fe05b8d921676721ab5e4b457efb7636886754
ron erlih <ronerlih@gmail.com>
2021-08-14 12:36:35 -0400

    pull request test

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import Project from "./components/Project";
 import logo from "./logo.svg";
-const App = 12;
 
+const App = 12;
 function App() {
    return (
       <>
