dc7b8720858e8b509e71a5b03c66f811ca8a36f5
ron erlih <ronerlih@gmail.com>
2021-08-14 08:17:16 -0400

    pr test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Project from "./components/Project";
+import logo from "./logo.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <>
+         <Header />
+         <Nav />
+         <Project />
+         <Footer />
+      </>
+   );
 }
 
 export default App;
